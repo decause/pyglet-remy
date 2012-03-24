0cf091335f257c23c34a3e8e7303d312f2612518
Remy D <remyd@civx.us>
2012-03-24 14:34:04 -0400

    PEP8's

--- examples/timer.py
+++ examples/timer.py
@@ -3,14 +3,14 @@
 # pyglet
 # Copyright (c) 2006-2008 Alex Holkner
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions 
+# modification, are permitted provided that the following conditions
 # are met:
 #
 #  * Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above copyright 
+#  * Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in
 #    the documentation and/or other materials provided with the
 #    distribution.
@@ -31,6 +31,8 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+#
+# This is the one I use the most of all the example code. Love this tool.
 # ----------------------------------------------------------------------------
 
 '''A full-screen minute:second timer.  Leave it in charge of your conference
@@ -46,9 +48,10 @@ import pyglet
 
 window = pyglet.window.Window(fullscreen=True)
 
+
 class Timer(object):
     def __init__(self):
-        self.label = pyglet.text.Label('00:00', font_size=400, 
+        self.label = pyglet.text.Label('00:00', font_size=400,
                                        x=window.width//2, y=window.height//2,
                                        anchor_x='center', anchor_y='center')
         self.reset()
@@ -71,6 +74,7 @@ class Timer(object):
             if m >= 4:
                 self.label.color = (180, 110, 50, 255)
 
+
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == pyglet.window.key.SPACE:
@@ -84,6 +88,7 @@ def on_key_press(symbol, modifiers):
     elif symbol == pyglet.window.key.ESCAPE:
         window.close()
 
+
 @window.event
 def on_draw():
     window.clear()
@@ -92,4 +97,3 @@ def on_draw():
 timer = Timer()
 pyglet.clock.schedule_interval(timer.update, 1)
 pyglet.app.run()
-
